c528723a7c34cd4dee7e83fb9d80fb776f92a9a7
aabebe <aabebe@mum.edu>
2019-04-17 14:59:41 -0500

    springboot start up web, containing the autoconfig dependencies and

---
+++ springboot-in-10-steps/src/main/java/com/springboot/basics/springbootin10steps/controller/BookController.java
@@ -0,0 +1,17 @@
+package com.springboot.basics.springbootin10steps.controller;
+
+import com.springboot.basics.springbootin10steps.domain.Book;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BookController {
+    @GetMapping("/books")
+    public List<Book> getAllBooks(){
+        return Arrays.asList(new Book(11,"Master Spring","Andualem Abebe"));
+    }
+
+}
